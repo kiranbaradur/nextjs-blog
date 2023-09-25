fd46d67e4ffc1367d8b298888ec3dd34805374a0
kiranbaradur-nt <kiran.baradur@nextuple.com>
2023-09-25 15:25:31 +0530

    posts, categories added

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe - </span>
+              <span className={styles.date}>11.12.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1>Categories</h1>
+      <div className={styles.categoryList}>
+        <Link href="/" className={`${styles.categoryItem} ${styles.style}`}>
+          Style
+        </Link>
+      </div>
+
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1>Editors Pick</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe - </span>
+              <span className={styles.date}>11.12.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
